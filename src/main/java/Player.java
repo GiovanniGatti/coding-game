193530b14db67aab614e3b8cc2bd4eda16a4694b
ggattipinheiro <ggattipinheiro@mediametrie.fr>
2016-05-03 18:02:52 +0200

    Block placement simulator

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,10 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Auto-generated code below aims at helping you parseLine
  * the standard input according to the problem statement.
  **/
-class Player {
+final class Player {
 
     static final int NUMBER_OF_COLUNMS = 6;
     static final int NUMBER_OF_ROWS = 12;
@@ -38,7 +47,29 @@ class Player {
         }
     }
 
-    private static class Block {
+    // TODO: fix this method
+    // 1- should not mutate current grid
+    // 2- maybe make it part of the Grid class?
+
+    static class ScoreEvaluation {
+        private final Grid nextState;
+        private final int score;
+
+        ScoreEvaluation(Grid nextState, int score) {
+            this.nextState = nextState;
+            this.score = score;
+        }
+
+        public Grid getNextState() {
+            return nextState;
+        }
+
+        public int getScore() {
+            return score;
+        }
+    }
+
+    static class Block {
         private final int a, b;
 
         Block(int a, int b) {
@@ -56,12 +87,24 @@ class Player {
     }
 
     static class Grid {
+        final int numberOfLines;
         final int numberOfColumns;
 
-        private final Map<Cell, Field> grid = new HashMap<>();
+        private final Map<Cell, Field> grid;
+        private final Map<Field, Set<Cell>> mirrorGrid;
 
-        Grid(int numberOfColumns) {
+        Grid(int numberOfLines, int numberOfColumns) {
+            this.numberOfLines = numberOfLines;
             this.numberOfColumns = numberOfColumns;
+            this.grid = new HashMap<>();
+            this.mirrorGrid = new HashMap<>();
+        }
+
+        Grid(Grid current) {
+            this.numberOfLines = current.numberOfLines;
+            this.numberOfColumns = current.numberOfColumns;
+            this.grid = deepCopy(current.grid);
+            this.mirrorGrid = mirrorDeepCopy(current.mirrorGrid);
         }
 
         void parseLine(int i, String line) {
@@ -70,18 +113,102 @@ class Player {
                         "but found " + line.length() + " instead.");
             }
 
+            if (i < 0 || i >= numberOfLines) {
+                throw new IllegalStateException("Invalid index " + i + " for line <" + line + ">. " +
+                        "Maximum number of lines allowed is " + numberOfLines);
+            }
+
             for (int j = 0; j < numberOfColumns; j++) {
                 Cell cell = new Cell(i, j);
-                grid.put(cell, Field.of(line.charAt(j)));
+                Field field = Field.of(line.charAt(j));
+                grid.put(cell, field);
+                if (mirrorGrid.containsKey(field)) {
+                    mirrorGrid.get(field).add(cell);
+                } else {
+                    mirrorGrid.put(field, new HashSet<>(Collections.singletonList(cell)));
+                }
             }
         }
 
         Field getField(int i, int j) {
             return grid.get(new Cell(i, j));
         }
+
+        Set<Cell> getCells(char value) {
+            return mirrorGrid.get(Field.of(value));
+        }
+
+        Cell nextAvailableCell(int column) {
+            for (int i = 0; i < numberOfLines; i++) {
+                if (grid.get(new Cell(i, column)).getType() != Field.Type.EMPTY) {
+                    return new Cell(i - 1, column);
+                }
+            }
+
+            return new Cell(numberOfLines - 1, column);
+        }
+
+        // TODO: deal with grouping
+        // TODO: count score
+        ScoreEvaluation place(Block block, int pos) {
+            Grid next = new Grid(this);
+            Cell cell = next.nextAvailableCell(pos);
+            Cell upper = new Cell(cell.row - 1, cell.column);
+
+            Field a = Field.of(block.getA());
+            Field b = Field.of(block.getB());
+
+            next.grid.put(cell, b);
+            next.grid.put(upper, a);
+
+            next.mirrorGrid.putIfAbsent(b, new HashSet<>());
+            next.mirrorGrid.get(b).add(cell);
+
+            next.mirrorGrid.putIfAbsent(a, new HashSet<>());
+            next.mirrorGrid.get(a).add(upper);
+
+            group(next);
+
+            return new ScoreEvaluation(next, 1);
+        }
+
+        private static int group(Grid grid) {
+            System.out.println(grid.mirrorGrid.get(Field.of('2')));
+            return 0;
+        }
+
+        // TODO: solution may come from graph structure
+        private static Set<Cell> findGroup(Set<Cell> maybeGroup) {
+
+            return new HashSet<>();
+        }
+
+        private static Map<Cell, Field> deepCopy(Map<Cell, Field> actual) {
+            Map<Cell, Field> copy = new HashMap<>();
+
+            for (Entry<Cell, Field> entry : actual.entrySet()) {
+                copy.put(entry.getKey(), entry.getValue());
+            }
+
+            return copy;
+        }
+
+        private static Map<Field, Set<Cell>> mirrorDeepCopy(Map<Field, Set<Cell>> actual) {
+            Map<Field, Set<Cell>> copy = new HashMap<>();
+
+            for (Entry<Field, Set<Cell>> entry : actual.entrySet()) {
+                Field key = entry.getKey();
+                copy.put(key, new HashSet<>());
+                for (Cell cell : actual.get(key)) {
+                    copy.get(key).add(cell);
+                }
+            }
+
+            return copy;
+        }
     }
 
-    private static class Cell {
+    static class Cell {
         final int row;
         final int column;
 
@@ -92,8 +219,12 @@ class Player {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Cell cell = (Cell) o;
             return Objects.equals(row, cell.row) &&
                     Objects.equals(column, cell.column);
@@ -103,9 +234,19 @@ class Player {
         public int hashCode() {
             return Objects.hash(row, column);
         }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Cell{");
+            sb.append("row=").append(row);
+            sb.append(", column=").append(column);
+            sb.append('}');
+            return sb.toString();
+        }
     }
 
     static class Field {
+
         enum Type {
             COLOR, SKULL, EMPTY
         }
@@ -126,15 +267,51 @@ class Player {
             return type;
         }
 
+        static Field of(int value) {
+            if (value < 0 || value > 5) {
+                throw new IllegalStateException("Illegal input value: " + value);
+            }
+
+            return of(String.valueOf(value).charAt(0));
+        }
+
         static Field of(char value) {
             switch (value) {
-                case '.':
-                    return new Field(value, Type.EMPTY);
-                case '0':
-                    return new Field(value, Type.SKULL);
-                default:
-                    return new Field(value, Type.COLOR);
+            case '.':
+                return new Field(value, Type.EMPTY);
+            case '0':
+                return new Field(value, Type.SKULL);
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+                return new Field(value, Type.COLOR);
+            default:
+                throw new IllegalArgumentException("Unknown value: " + value);
             }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Field field = (Field) o;
+            return value == field.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
         }
     }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,8 +1,8 @@
-import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
 
 @RunWith(HierarchicalContextRunner.class)
 public class PlayerTest implements WithAssertions, GridAssert.WithTableAssertions {
@@ -11,22 +11,35 @@ public class PlayerTest implements WithAssertions, GridAssert.WithTableAssertion
 
         @Test
         public void throw_ISE_when_exceeding_columns() {
-            assertThatThrownBy(() -> new Player.Grid(1).parseLine(0, ".."))
+            assertThatThrownBy(() -> new Player.Grid(1, 1).parseLine(0, ".."))
                     .isInstanceOf(IllegalStateException.class)
                     .hasMessageContaining("Expected 1 columns, but found 2 instead.");
         }
 
         @Test
         public void throw_ISE_when_missing_columns() {
-            assertThatThrownBy(() -> new Player.Grid(2).parseLine(0, "."))
+            assertThatThrownBy(() -> new Player.Grid(1, 2).parseLine(0, "."))
                     .isInstanceOf(IllegalStateException.class)
                     .hasMessageContaining("Expected 2 columns, but found 1 instead.");
         }
 
+        @Test
+        public void throw_ISE_when_negative_line_input() {
+            assertThatThrownBy(() -> new Player.Grid(1, 1).parseLine(1, "."))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessageContaining("Invalid index 1 for line <.>. Maximum number of lines allowed is 1");
+        }
+
+        @Test
+        public void throw_ISE_when_too_many_input_lines() {
+            assertThatThrownBy(() -> new Player.Grid(1, 1).parseLine(1, "."))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessageContaining("Invalid index 1 for line <.>. Maximum number of lines allowed is 1");
+        }
 
         @Test
         public void load_table() {
-            Player.Grid grid = new Player.Grid(4);
+            Player.Grid grid = new Player.Grid(3, 4);
             grid.parseLine(0, "..0.");
             grid.parseLine(1, ".1..");
             grid.parseLine(2, ".22.");
@@ -37,6 +50,97 @@ public class PlayerTest implements WithAssertions, GridAssert.WithTableAssertion
                     ".22.");
         }
 
+        @Test
+        public void load_mirrored_cells() {
+            Player.Grid grid = new Player.Grid(3, 4);
+            grid.parseLine(0, "..0.");
+            grid.parseLine(1, ".1..");
+            grid.parseLine(2, ".22.");
+
+            assertThat(grid.getCells('0')).containsExactlyInAnyOrder(new Player.Cell(0, 2));
+            assertThat(grid.getCells('1')).containsExactlyInAnyOrder(new Player.Cell(1, 1));
+            assertThat(grid.getCells('2')).containsExactlyInAnyOrder(new Player.Cell(2, 1), new Player.Cell(2, 2));
+        }
+
+        @Test
+        public void find_columns_next_available_cell_non_empty_cell(){
+            Player.Grid grid = new Player.Grid(3, 1);
+            grid.parseLine(0, ".");
+            grid.parseLine(1, "1");
+            grid.parseLine(2, "2");
+
+            Player.Cell available = grid.nextAvailableCell(0);
+            assertThat(available).isEqualTo(new Player.Cell(0, 0));
+        }
+
+        @Test
+        public void find_columns_next_available_cell_on_empty_column(){
+            Player.Grid grid = new Player.Grid(3, 1);
+            grid.parseLine(0, ".");
+            grid.parseLine(1, ".");
+            grid.parseLine(2, ".");
+
+            Player.Cell available = grid.nextAvailableCell(0);
+            assertThat(available).isEqualTo(new Player.Cell(2, 0));
+        }
     }
 
+    @Test
+    public void place_non_matching_block() {
+        Player.Grid grid = new Player.Grid(3, 4);
+        grid.parseLine(0, "....");
+        grid.parseLine(1, "....");
+        grid.parseLine(2, ".222");
+
+        Player.Block block = new Player.Block(1, 1);
+
+        Player.ScoreEvaluation score = grid.place(block, 0);
+
+        assertThat(score.getNextState()).isEqualTo(
+                "....",
+                "1...",
+                "1222");
+    }
+
+    @Test
+    public void group_block_after_placement() {
+        Player.Grid grid = new Player.Grid(3, 4);
+        grid.parseLine(0, "....");
+        grid.parseLine(1, "..2.");
+        grid.parseLine(2, "..2.");
+
+        Player.Block block = new Player.Block(2, 2);
+
+        Player.ScoreEvaluation score = grid.place(block, 1);
+
+        assertThat(score.getNextState()).isEqualTo(
+                "....",
+                "....",
+                "....");
+    }
+//
+//    @Test
+//    public void do_match_vertical_line_group() {
+//        Player.Grid grid = new Player.Grid(4);
+//        grid.parseLine(0, "..2.");
+//        grid.parseLine(1, "..2.");
+//        grid.parseLine(2, "..2.");
+//        grid.parseLine(3, "..2.");
+//    }
+//
+//    @Test
+//    public void do_match_horizontal_line_group() {
+//        Player.Grid grid = new Player.Grid(4);
+//        grid.parseLine(0, "....");
+//        grid.parseLine(1, "2222");
+//    }
+//
+//    @Test
+//    public void do_match_s_line_group() {
+//        Player.Grid grid = new Player.Grid(4);
+//        grid.parseLine(0, "....");
+//        grid.parseLine(1, "....");
+//        grid.parseLine(2, "....");
+//        grid.parseLine(3, "2222");
+//    }
 }
